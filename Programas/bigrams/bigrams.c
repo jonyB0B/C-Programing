26c3ad21141ef6717f3f7e5b3001a8e5e4902b7d
jonyB0B <sp.jona@gmail.com>
2015-11-18 14:17:54 +0100

    update

--- Programas/bigrams/bigrams.c
+++ Programas/bigrams/bigrams.c
@@ -8,8 +8,35 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+
+static void
+run(char *file){
+  FILE *fp;
+  struct t_reg r;
+
+  fp=fopen (file,"r");
+  if (fp == NULL){
+    err(1,"fopen\n " );
+  }
+  fread (&r, sizeof(r), 1, fp);
+  while (!feof(fp)) {
+    printf ("%d: %s: %c\n" , r.num, r.cad, r.car);
+    fread (&r, sizeof(r), 1, fp);
+  }
+  fclose (fp);
+}
+
+
 int
 main(int argc, char* argv[] ){
+  char *file;
 
+  if (argc<2){
+    fprintf(stderr,"Numero de argumentos incorrecto\n");
+    exit(1);
+  }
+  file = argv[1];
+
+  run(file);
   exit (EXIT_SUCCESS);
 }
